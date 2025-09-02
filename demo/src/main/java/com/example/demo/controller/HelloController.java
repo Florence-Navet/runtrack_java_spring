58776e3992beb271e3257a18366829e10ad129b4
Florence <enolaworld@gmail.com>
2025-09-02 20:59:50 +0200

    feat: rechargement automatiquement done

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,14 +16,17 @@ public class HelloController {
     private String toolsMessage;
 
     @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", greetingMessage);
+        return "index";
+    }
+
+    @GetMapping("/hello")
     @ResponseBody
     public String hello() {
+        System.out.println("Forcer le redémarrage DevTools");
+        System.out.println("DevTools est top nul");
 
         return greetingMessage + " | " + toolsMessage;
     }
 }
-
-
-
-
-
