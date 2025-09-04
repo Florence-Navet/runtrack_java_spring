2f472b019945a8fad0248d154f50b8e5796a590e
AeRiaSaito <florence.navet@laplateforme.io>
2025-09-04 11:54:40 +0200

    feat: creation class personn

---
+++ demo/src/main/java/com/example/demo/Model/Person.java
@@ -0,0 +1,34 @@
+package com.example.demo.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue()
+    private long id;
+    private String name;
+    private Integer age;
+
+
+public Person(String name, Integer age) {
+    this.name = name;
+    this.age = age;
+}
+
+public Long getId() { return id; }
+
+public String getName() { return name; }
+public void setName(String name) { this.name = name; }
+
+public Integer getAge() { return age; }
+public void setAge(Integer age) { this.age = age; }
+
+public String toString() {
+    return "Person{id=" + id + ", name='" + name + "', age=" + age + "}";
+}
+}
+
